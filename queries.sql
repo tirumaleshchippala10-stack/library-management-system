104cec9de13f6ddb6a110679d0699e9a50412263
tirumaleshchippala10-stack <tirumaleshchippala10@gmail.com>
2026-06-28 11:17:19 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,1 +1,56 @@
+-- ===========================================
+-- Library Management System Queries
+-- ===========================================
 
+-- 1. Display all books
+
+SELECT * FROM Books;
+
+-- 2. Display all members
+
+SELECT * FROM Members;
+
+-- 3. Show books with their authors
+
+SELECT
+    Books.title,
+    Authors.author_name
+FROM Books
+JOIN Authors
+ON Books.author_id = Authors.author_id;
+
+-- 4. Show which member borrowed which book
+
+SELECT
+    Members.member_name,
+    Books.title,
+    Loans.issue_date,
+    Loans.due_date,
+    Loans.return_date
+FROM Loans
+JOIN Members
+ON Loans.member_id = Members.member_id
+JOIN Books
+ON Loans.book_id = Books.book_id;
+
+-- 5. Show books that are not yet returned
+
+SELECT
+    Members.member_name,
+    Books.title
+FROM Loans
+JOIN Members
+ON Loans.member_id = Members.member_id
+JOIN Books
+ON Loans.book_id = Books.book_id
+WHERE return_date IS NULL;
+
+-- 6. Count total books
+
+SELECT COUNT(*) AS TotalBooks
+FROM Books;
+
+-- 7. Count total members
+
+SELECT COUNT(*) AS TotalMembers
+FROM Members;
